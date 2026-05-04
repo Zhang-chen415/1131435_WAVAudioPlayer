3ceba248d4974cd56932305e9f4d6cc2cea7f447
Zhang-chen415 <ba1973216@gmail.com>
2026-05-04 21:12:00 +0800

    完成PPT內容

--- 1131435_張新誠_WAV音效播放器/Form1.Designer.cs
+++ 1131435_張新誠_WAV音效播放器/Form1.Designer.cs
@@ -31,24 +31,36 @@
             this.grPath = new System.Windows.Forms.GroupBox();
             this.grpButton = new System.Windows.Forms.GroupBox();
             this.txtPath = new System.Windows.Forms.TextBox();
+            this.btnBrowse = new System.Windows.Forms.Button();
+            this.btnPlay = new System.Windows.Forms.Button();
+            this.btnLoop = new System.Windows.Forms.Button();
+            this.btnStop = new System.Windows.Forms.Button();
+            this.btnEnd = new System.Windows.Forms.Button();
+            this.ofdWAVFile = new System.Windows.Forms.OpenFileDialog();
             this.grPath.SuspendLayout();
+            this.grpButton.SuspendLayout();
             this.SuspendLayout();
             // 
             // grPath
             // 
+            this.grPath.Controls.Add(this.btnBrowse);
             this.grPath.Controls.Add(this.txtPath);
             this.grPath.Location = new System.Drawing.Point(13, 13);
             this.grPath.Name = "grPath";
-            this.grPath.Size = new System.Drawing.Size(633, 78);
+            this.grPath.Size = new System.Drawing.Size(481, 78);
             this.grPath.TabIndex = 0;
             this.grPath.TabStop = false;
             this.grPath.Text = "音效位置";
             // 
             // grpButton
             // 
+            this.grpButton.Controls.Add(this.btnEnd);
+            this.grpButton.Controls.Add(this.btnStop);
+            this.grpButton.Controls.Add(this.btnLoop);
+            this.grpButton.Controls.Add(this.btnPlay);
             this.grpButton.Location = new System.Drawing.Point(12, 119);
             this.grpButton.Name = "grpButton";
-            this.grpButton.Size = new System.Drawing.Size(634, 77);
+            this.grpButton.Size = new System.Drawing.Size(482, 77);
             this.grpButton.TabIndex = 1;
             this.grpButton.TabStop = false;
             this.grpButton.Text = "播放按鍵";
@@ -57,20 +69,76 @@
             // 
             this.txtPath.Location = new System.Drawing.Point(16, 34);
             this.txtPath.Name = "txtPath";
-            this.txtPath.Size = new System.Drawing.Size(497, 25);
+            this.txtPath.Size = new System.Drawing.Size(344, 25);
             this.txtPath.TabIndex = 0;
+            // 
+            // btnBrowse
+            // 
+            this.btnBrowse.Location = new System.Drawing.Point(371, 28);
+            this.btnBrowse.Name = "btnBrowse";
+            this.btnBrowse.Size = new System.Drawing.Size(88, 33);
+            this.btnBrowse.TabIndex = 1;
+            this.btnBrowse.Text = "瀏覽";
+            this.btnBrowse.UseVisualStyleBackColor = true;
+            this.btnBrowse.Click += new System.EventHandler(this.btnBrowse_Click);
+            // 
+            // btnPlay
+            // 
+            this.btnPlay.Location = new System.Drawing.Point(31, 25);
+            this.btnPlay.Name = "btnPlay";
+            this.btnPlay.Size = new System.Drawing.Size(82, 33);
+            this.btnPlay.TabIndex = 2;
+            this.btnPlay.Text = "播放一次";
+            this.btnPlay.UseVisualStyleBackColor = true;
+            this.btnPlay.Click += new System.EventHandler(this.btnPlay_Click);
+            // 
+            // btnLoop
+            // 
+            this.btnLoop.Location = new System.Drawing.Point(142, 25);
+            this.btnLoop.Name = "btnLoop";
+            this.btnLoop.Size = new System.Drawing.Size(82, 33);
+            this.btnLoop.TabIndex = 3;
+            this.btnLoop.Text = "重複播放";
+            this.btnLoop.UseVisualStyleBackColor = true;
+            this.btnLoop.Click += new System.EventHandler(this.btnLoop_Click);
+            // 
+            // btnStop
+            // 
+            this.btnStop.Location = new System.Drawing.Point(258, 25);
+            this.btnStop.Name = "btnStop";
+            this.btnStop.Size = new System.Drawing.Size(82, 33);
+            this.btnStop.TabIndex = 4;
+            this.btnStop.Text = "停止播放";
+            this.btnStop.UseVisualStyleBackColor = true;
+            this.btnStop.Click += new System.EventHandler(this.btnStop_Click);
+            // 
+            // btnEnd
+            // 
+            this.btnEnd.Location = new System.Drawing.Point(372, 25);
+            this.btnEnd.Name = "btnEnd";
+            this.btnEnd.Size = new System.Drawing.Size(82, 33);
+            this.btnEnd.TabIndex = 5;
+            this.btnEnd.Text = "結束程式";
+            this.btnEnd.UseVisualStyleBackColor = true;
+            this.btnEnd.Click += new System.EventHandler(this.btnEnd_Click);
+            // 
+            // ofdWAVFile
+            // 
+            this.ofdWAVFile.DefaultExt = "wav";
             // 
             // frmWAVPlayer
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 15F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(658, 216);
+            this.ClientSize = new System.Drawing.Size(511, 216);
             this.Controls.Add(this.grpButton);
             this.Controls.Add(this.grPath);
             this.Name = "frmWAVPlayer";
             this.Text = "WAV音效播放器";
+            this.FormClosing += new System.Windows.Forms.FormClosingEventHandler(this.frmWAVPlayer_FormClosing);
             this.grPath.ResumeLayout(false);
             this.grPath.PerformLayout();
+            this.grpButton.ResumeLayout(false);
             this.ResumeLayout(false);
 
         }
@@ -80,6 +148,12 @@
         private System.Windows.Forms.GroupBox grPath;
         private System.Windows.Forms.TextBox txtPath;
         private System.Windows.Forms.GroupBox grpButton;
+        private System.Windows.Forms.Button btnBrowse;
+        private System.Windows.Forms.Button btnEnd;
+        private System.Windows.Forms.Button btnStop;
+        private System.Windows.Forms.Button btnLoop;
+        private System.Windows.Forms.Button btnPlay;
+        private System.Windows.Forms.OpenFileDialog ofdWAVFile;
     }
 }
 
